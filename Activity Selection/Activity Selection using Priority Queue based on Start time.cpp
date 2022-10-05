c6fdf44e4438a9b9e653c471ba4e50ca8fae49b2
Piyushmudgal <72311008+Piyushmudgal@users.noreply.github.com>
2022-10-05 14:34:44 +0530

    Add files via upload

---
+++ Activity Selection/Activity Selection using Priority Queue based on Start time.cpp
@@ -0,0 +1,77 @@
+/*
+Activity Selection using Priority Queue based on Start time
+Given a set ‘S’ of ‘n’ activities, implement the iterative greedy algorithm to select a subset of activities from S by selecting the task that starts last. Build a priority queue (max heap) based on start time and then select the activities
+
+Input Format
+
+First line contains the number of activities, n
+
+Next 'n' line contains the details of the activities such as name of activity, start time and finish time
+
+Output Format
+
+Print the name of the activities that are selected separated by a space
+*/
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+class activity{
+    public:
+        string name;
+        int start_time;
+        int end_time;
+        friend istream& operator>>(istream&, activity&);
+        friend ostream& operator<<(ostream&, const activity&);
+        bool operator<(const activity&) const;
+};
+istream& operator>>(istream& in, activity& a){
+    in>>a.name>>a.start_time>>a.end_time;
+    return in;
+}
+ostream& operator<<(ostream& out, activity& a){
+    out<<a.name<<" ";
+    return out;
+}
+bool activity::operator<(const activity& a) const{
+    return a.start_time > start_time;
+}
+void activitySelectionIterative(priority_queue<activity>& pq, vector<activity>& res){
+    activity a;
+    a = pq.top();
+    res.push_back(a);
+    pq.pop();
+    while(!pq.empty()){
+        if(pq.top().end_time <= a.start_time){
+            a = pq.top();
+            res.push_back(a);
+        }
+        pq.pop();
+    }
+    return;
+}
+
+int main(){
+    priority_queue<activity> pq;
+    vector<activity> res;
+    int n;
+    cin>>n;
+    activity a;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>a;
+        pq.push(a);
+    }
+    // while(!pq.empty()){
+    //     a = pq.top();
+    //     cout<<a;
+    //     pq.pop();
+    // }
+    activitySelectionIterative(pq, res);
+    reverse(res.begin(), res.end());
+    for(auto i: res){
+        cout<< i;
+    }
+}
---
+++ Activity Selection/Activity_Selection_Sort_Recursive.cpp
@@ -0,0 +1,110 @@
+/*
+Activity Selection Sort Recursive
+Given a set ‘S’ of ‘n’ activities, implement the recursive greedy algorithm to select a subset of activities from S by selecting the task that finishes first. Sort the activities in ascending order based on finsih time and then select the activities
+
+Input Format
+
+First line contains the number of activities, n
+
+Next 'n' line contains the details of the activities such as name of activity, start time and finish time
+
+Output Format
+
+Print the name of the activities that are selected separated by a space
+*/
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+class activity{
+    public:
+        string name;
+        int start_time;
+        int end_time;
+        friend istream& operator>>(istream&, activity&);
+        friend ostream& operator<<(ostream&, const activity&);
+        bool operator<(const activity&) const;
+};
+istream& operator>>(istream& in, activity& a){
+    in>>a.name>>a.start_time>>a.end_time;
+    return in;
+}
+ostream& operator<<(ostream& out, activity& a){
+    out<<a.name<<" ";
+    return out;
+}
+bool activity::operator<(const activity& a) const{
+    return a.end_time > end_time;
+}
+void merge(vector<activity> &arr,int left, int mid, int right)
+{
+    int leftLength=mid-left+1;
+    int rightLength=right-mid;
+    activity leftArray[leftLength],rightArray[rightLength];
+    for(int i=0;i<leftLength;i++)
+        leftArray[i]=arr[left+i];
+
+    for (int j = 0; j < rightLength; j++)
+        rightArray[j]=arr[mid+1+j];
+    
+    int i=0,j=0,k=left;
+    while(i<leftLength && j<rightLength)
+    {
+        if(leftArray[i]<rightArray[j])
+            arr[k++]=leftArray[i++];
+        
+        else 
+            arr[k++]=rightArray[j++];
+    }
+    while(i<leftLength || j<rightLength)
+    {
+        if(i<leftLength)
+            while(i<leftLength)
+                arr[k++]=leftArray[i++];
+        if(j<rightLength)
+            while(j<rightLength)
+                arr[k++]=rightArray[j++];
+    }
+}
+void mergeSort(vector<activity> &arr,int left, int right)
+{
+    if(left>=right)
+        return;
+    int mid=(left+right)/2;
+    mergeSort(arr,left,mid);
+    mergeSort(arr,mid+1,right);
+    merge(arr,left, mid, right);
+
+}
+void activitySelectionRecursive(vector<activity>& v, vector<activity>& res, int i){
+    activity a;
+    a = v[i++];
+    res.push_back(a);
+    // cout<<a;
+    while(i < v.size() && v[i].start_time < a.end_time){
+        i++;
+    }
+    if(i < v.size()){
+        activitySelectionRecursive(v, res, i);
+    }
+    else
+        return;
+}
+int main(){
+    vector<activity> v;
+    activity a;
+    int n;
+    cin>>n;
+    for(int i = 0; i < n; i++){
+        cin>>a;
+        v.push_back(a);
+    }
+    mergeSort(v, 0, v.size() - 1);
+    cout<<endl;
+    vector<activity> res;
+    activitySelectionRecursive(v, res, 0);
+    for(auto i:res){
+        cout<<i;
+    }
+    cout<<endl;
+}
